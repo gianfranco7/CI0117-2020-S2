82cf4b3a80d00cdeed238fe2a98eabd16f09ddc9
gianfranco7 <gianfrancobagnarello@hotmail.com>
2020-10-07 01:12:07 -0600

    solucion serial de la 1 del examen

---
+++ examenes/B70866_GianfrancoBagnarelloHernandez_Examen1/dna_sequences.h
@@ -0,0 +1,5 @@
+#ifndef DNA_SEQUENCES_H
+#define DNA_SEQUENCES_H
+const int first_sequence[26] = {0,4,2,5,1,5,6,9,2,9,10,11,10,13,14,15,16,17,18,20,20,21,22,14,24}; 
+const int second_sequence[26] = {0,4,2,5,1,5,6,9,2,9,10,11,10,13,14,15,16,17,18,20,20,21,22,14,24};
+#endif 
