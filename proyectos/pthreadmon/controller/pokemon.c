42159202bca5a1b3339374a5fb6097bda17eaa2c
gianfranco7 <gianfrancobagnarello@hotmail.com>
2020-10-18 20:54:56 -0600

    Refactorizado todo el código. El main ya se ve decente. Funcion para validación de entradas lista. Falta implementar el algoritmo de fight(), pero ya está diseñado.

--- proyectos/pthreadmon/controller/pokemon.c
+++ proyectos/pthreadmon/controller/pokemon.c
@@ -19,36 +19,52 @@ double walltime_elapsed(const walltime_t *start)
 	return elapsed;
 }
 
-bool validate_pokemon(int *array, int pokemon_id)
+void allocate_dynamic_memory()
+{
+	shared_data = (shared_data_t *)calloc(1, sizeof(shared_data_t));
+	p0_pokemon = malloc((size_t)(AMOUNT_OF_POKEMON * sizeof(pthread_t)));
+	p1_pokemon = malloc((size_t)(AMOUNT_OF_POKEMON * sizeof(pthread_t)));
+	p0_pokemon_data_list = malloc((size_t)(AMOUNT_OF_POKEMON * sizeof(pokemon_data_t)));
+	p1_pokemon_data_list = malloc((size_t)(AMOUNT_OF_POKEMON * sizeof(pokemon_data_t)));
+}
+
+int validate_pokemon(int scanned_id, int * validation_array)
 {
 	for (int i = 0; i < AMOUNT_OF_POKEMON; ++i)
 	{
-		if (array[i] == pokemon_id)
+		if (validation_array[i] == scanned_id)
 		{
-			return false;
+			return 1;
 		}
 	}
-	return true;
+	return 0;
+}
+
+void get_player_pokemon(pokemon_data_t * pokemon_data_list)
+{
+	int validation_array[AMOUNT_OF_POKEMON] = {0,0,0};
+	int scanned_id = 0;
+	for(int i = 0; i < AMOUNT_OF_POKEMON; ++i)
+	{
+		printf("Select pokémon %d: ", i);
+		scanf("%d", &scanned_id);
+		while(validate_pokemon(scanned_id, validation_array) == 1)
+		{
+			printf("Please select a different Pokémon species\n");
+			printf("Select pokémon %d: ", i);
+			scanf("%d", &scanned_id);
+		}
+		validation_array[i] = scanned_id;
+		pokemon_data_list[i].id = scanned_id;
+ 	}
 }
 
 void get_pokemon()
 {
-	for (int i = 0; i < 2; ++i)
-	{
-		printf("Please PLAYER %d enter POKEMON\n", i);
-		for (int j = 0; j < AMOUNT_OF_POKEMON; ++j)
-		{
-			printf("%d: ", j);
-			if (i == 0)
-			{
-				scanf("%d", &p1_pokemon_data_list[j].id);
-			}
-			if (i == 1)
-			{
-				scanf("%d", &p2_pokemon_data_list[j].id);
-			}
-		}
-	}
+	printf("PLAYER 1. Select 3 different pokémon species by typing each ID\n");
+	get_player_pokemon(p0_pokemon_data_list);
+	printf("Player 2. Select 3 different pokémon species by typing each ID\n");
+	get_player_pokemon(p1_pokemon_data_list);
 }
 
 void print_pokemon()
@@ -61,15 +77,14 @@ void print_pokemon()
 			printf("%d ", j);
 			if (i == 0)
 			{
-				printf(" name: %s\n", get_pokemon_species_name(p1_pokemon_data_list[j].id));
+				printf(" name: %s\n", get_pokemon_species_name(p0_pokemon_data_list[j].id));
 			}
 			if (i == 1)
 			{
-				printf(" name: %s\n", get_pokemon_species_name(p2_pokemon_data_list[j].id));
+				printf(" name: %s\n", get_pokemon_species_name(p1_pokemon_data_list[j].id));
 			}
 		}
 	}
-	printf("PRINT POKEMON CHECKPOINT\n");
 }
 
 void start_battle_time()
@@ -83,10 +98,55 @@ double get_battle_time()
 	return elapsed;
 }
 
-void begin_battle()
+void initialize_mutexes(pokemon_data_t * pokemon_data_list)
 {
-	printf("------------BEGIN BATTLE!!!------------\n");
-	start_battle_time();
+	for(int i = 0; i < AMOUNT_OF_POKEMON; ++i)
+	{
+		pthread_mutex_init(&pokemon_data_list[i].my_mutex, NULL);
+	}
+}
+
+void lock_mutexes(pokemon_data_t * pokemon_data_list)
+{
+	for(int i = 1; i < AMOUNT_OF_POKEMON; ++i)
+	{
+		pthread_mutex_lock(&pokemon_data_list[i].my_mutex);
+	}
+}
+
+void assign_shared_data(shared_data_t * shared_data, pokemon_data_t * pokemon_data_list)
+{
+	pokemon_data_list->shared_data = shared_data;
+}
+
+void assign_active_pokemon_num(shared_data_t * shared_data, int player_num, int pokemon_num)
+{
+	if(player_num ==  0)
+	{
+		shared_data->active_p0_num = pokemon_num;
+	}
+	if(player_num == 1)
+	{
+		shared_data->active_p1_num = pokemon_num;
+	}
+}
+
+void initialize_game()
+{
+	allocate_dynamic_memory();
+	pthread_barrier_init(&shared_data->barrier, NULL, 6);
+	initialize_mutexes(p0_pokemon_data_list);
+	initialize_mutexes(p1_pokemon_data_list);
+	initialize_data();
+	get_pokemon();
+	print_pokemon();
+	assign_shared_data(shared_data, p0_pokemon_data_list);
+	assign_shared_data(shared_data, p1_pokemon_data_list);
+	start_battle_time(&battle_time);
+	lock_mutexes(p0_pokemon_data_list);
+	lock_mutexes(p1_pokemon_data_list);
+	assign_active_pokemon_num(shared_data, 0, 0);
+	assign_active_pokemon_num(shared_data, 0, 1);
 }
 
 double calculate_effectiveness(int target_pokemon_type, int attacking_move_type)
@@ -110,7 +170,7 @@ double calculate_effectiveness(int target_pokemon_type, int attacking_move_type)
 		return 1;
 	}
 }
-
+//duda con el bonus-siempre es 2.3?
 double calculate_damage(double power, double effectiveness, double bonus)
 {
 	return floor(power * effectiveness * bonus) + 1;;
@@ -121,62 +181,33 @@ void *fight(void *args)
 	pokemon_data_t *pokemon_data = (pokemon_data_t *)args;
 	shared_data_t *shared_data = pokemon_data->shared_data;
 	pthread_barrier_wait(&shared_data->barrier);
+
 	pthread_mutex_lock(&pokemon_data->my_mutex);
+
 	walltime_start(&pokemon_data->time_lived);
+
 	while (pokemon_data->hp > 0)
 	{
-		if (pokemon_data->power > get_move_energy(get_pokemon_charged_move_id(pokemon_data->id)))
-		{	//ataque cargado
-			if(pokemon_data->player_num == 0)
-			{
-				double move_energy = get_move_energy(get_pokemon_charged_move_id(pokemon_data->id));
-				double effectiveness = calculate_effectiveness(get_pokemon_type_id(battle_zone->pk2.id),
-				get_pokemon_type_id(pokemon_data->id));
-				double damage = calculate_damage(move_energy,effectiveness,ATTACK_BONUS);
-				battle_zone->pk2.hp-=damage;
-				usleep(get_move_cooldown(get_pokemon_charged_move_id(pokemon_data->id)));
-			}else
-			{
-				double move_energy = get_move_energy(get_pokemon_charged_move_id(pokemon_data->id));
-				double effectiveness = calculate_effectiveness(get_pokemon_type_id(battle_zone->pk1.id),
-				get_pokemon_type_id(pokemon_data->id));
-				double damage = calculate_damage(move_energy,effectiveness,ATTACK_BONUS);
-				battle_zone->pk1.hp-=damage;
-				usleep(get_move_cooldown(get_pokemon_charged_move_id(pokemon_data->id)));
-			}
-		}
-		else	//ataque rapido
-		{
-			double power = get_move_energy(get_pokemon_fast_move_id(pokemon_data->id));
-			if(pokemon_data->player_num == 0)
-			{
-				double move_energy = get_move_energy(get_pokemon_fast_move_id(pokemon_data->id));
-				double effectiveness = calculate_effectiveness(get_pokemon_type_id(battle_zone->pk2.id),
-				get_pokemon_type_id(pokemon_data->id));
-				double damage = calculate_damage(move_energy,effectiveness,ATTACK_BONUS);
-				battle_zone->pk2.hp-=damage;
-				usleep(get_move_cooldown(get_pokemon_fast_move_id(pokemon_data->id)));
-			}else
-			{
-				double move_energy = get_move_energy(get_pokemon_fast_move_id(pokemon_data->id));
-				double effectiveness = calculate_effectiveness(get_pokemon_type_id(battle_zone->pk1.id),
-				get_pokemon_type_id(pokemon_data->id));
-				double damage = calculate_damage(move_energy,effectiveness,ATTACK_BONUS);
-				battle_zone->pk1.hp-=damage;
-				usleep(get_move_cooldown(get_pokemon_fast_move_id(pokemon_data->id)));
-			}
-		}
-	}
-	//desbloquear el mutex del siguiente
-	walltime_elapsed(&pokemon_data->time_lived);
-	if(pokemon_data->player_num == 0 && pokemon_data->player_num != 2){
-		pthread_mutex_unlock(&p1_pokemon_data_list[pokemon_data->num+1].my_mutex);
-	}
-	if(pokemon_data->player_num == 0 && pokemon_data->player_num != 2)
-	{
-		pthread_mutex_unlock(&p2_pokemon_data_list[pokemon_data->num+1].my_mutex);
+
 	}
 	return NULL;
+}
+
+void create_threads()
+{
+	for (int i = 0; i < AMOUNT_OF_POKEMON; ++i)
+	{
+		p0_pokemon_data_list[i].shared_data = shared_data;
+		p0_pokemon_data_list[i].hp = STARTING_HEALTH;
+		p0_pokemon_data_list[i].power = STARTING_POWER;
+		p0_pokemon_data_list[i].player_num = 0;
+		p1_pokemon_data_list[i].shared_data = shared_data;
+		p1_pokemon_data_list[i].hp = STARTING_HEALTH;
+		p1_pokemon_data_list[i].power = STARTING_POWER;
+		p1_pokemon_data_list[i].player_num = 1;
+		pthread_create(&p0_pokemon[i], NULL, fight, (void *)&p0_pokemon_data_list[i]);
+		pthread_create(&p1_pokemon[i], NULL, fight, (void *)&p1_pokemon_data_list[i]);
+	}
 }
 
 void join_threads(pthread_t *threads, size_t thread_count)
@@ -187,18 +218,25 @@ void join_threads(pthread_t *threads, size_t thread_count)
 	}
 }
 
-void free_memory(pthread_t *p1threads, pthread_t *p2threads,
-				 shared_data_t *shared_data, pokemon_data_t *p1, pokemon_data_t *p2)
+void free_memory(pthread_t *p0threads, pthread_t *p1threads,
+				 shared_data_t *shared_data, pokemon_data_t *p0_list, pokemon_data_t *p1_list)
 {
+	free(p0threads);
 	free(p1threads);
-	free(p2threads);
 	free(shared_data);
-	free(p1);
-	free(p2);
+	free(p0_list);
+	free(p1_list);
 	for (int i = 0; i < AMOUNT_OF_POKEMON; ++i)
 	{
-		pthread_mutex_destroy(&shared_data->player1_mutexes[i]);
-		pthread_mutex_destroy(&shared_data->player2_mutexes[i]);
+		pthread_mutex_destroy(&p0_pokemon_data_list[i].my_mutex);
+		pthread_mutex_destroy(&p1_pokemon_data_list[i].my_mutex);
 	}
 	//pthread_barrier_destroy(&shared_data->barrier);
+}
+
+void finish_game()
+{
+	join_threads(p0_pokemon, AMOUNT_OF_POKEMON);
+	join_threads(p1_pokemon, AMOUNT_OF_POKEMON);
+	free_memory(p0_pokemon, p1_pokemon, shared_data, p0_pokemon_data_list, p1_pokemon_data_list);
 }
--- proyectos/pthreadmon/model/pokemon.h
+++ proyectos/pthreadmon/model/pokemon.h
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdbool.h>
 #include <pthread.h>
 #include <math.h>
 #include <unistd.h>
@@ -14,10 +13,9 @@ walltime_t battle_time;
 
 typedef struct
 {
-    int *validationArray;
     pthread_barrier_t barrier;
-    pthread_mutex_t *player1_mutexes;
-    pthread_mutex_t *player2_mutexes;
+    int active_p0_num;  //battle order num
+    int active_p1_num;  //battle order_num
 } shared_data_t;
 
 typedef struct
@@ -27,25 +25,14 @@ typedef struct
     int id;
     double hp;
     double power;
-    bool winner;
-    bool loser;
     walltime_t time_lived;
     pthread_mutex_t my_mutex;
     shared_data_t *shared_data;
 } pokemon_data_t;
 
-typedef struct
-{
-    int id_pk1;
-    int id_pk2;
-    pokemon_data_t pk1;
-    pokemon_data_t pk2;
-}battlefield_data_t;
-
 shared_data_t *shared_data;
-battlefield_data_t *battle_zone;
+pthread_t *p0_pokemon;
 pthread_t *p1_pokemon;
-pthread_t *p2_pokemon;
+pokemon_data_t *p0_pokemon_data_list;
 pokemon_data_t *p1_pokemon_data_list;
-pokemon_data_t *p2_pokemon_data_list;
 #endif
